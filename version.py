be05986008acbaa17c011cab06c3b8ec821ffc26
ynossiul <59929310+ynossiul@users.noreply.github.com>
2020-01-15 11:12:10 -0600

    Create version.py

---
+++ version.py
@@ -0,0 +1,7 @@
+import sys
+import plataform
+print("/n")
+print("Detalles Python: ")
+print(sys.version)
+print("/n")
+print(str("La version actual de Python es: ") + str(plataform.python_version())
