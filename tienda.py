23f103076b844dc4c7b0951a277ece395a86b342
ynossiul <59929310+ynossiul@users.noreply.github.com>
2020-01-15 11:43:18 -0600

    Create tienda.py

---
+++ tienda.py
@@ -0,0 +1,4 @@
+print("Escribe los siguientes valores solicitados")
+nombre1=input("Ingrese el Nombre del primer producto")
+precio=int(input("Ingresa el primer precio"))
+print(f"El nombre del producto es {nombre1} y el precio es {precio} pesos")
